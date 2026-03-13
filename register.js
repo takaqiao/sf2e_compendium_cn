da68d24cf4586083335412208e8721f356f541c9
takaqiao <532236728@qq.com>
2026-03-13 22:56:01 +0800

    rename module id to sf2ecompendium and bump 0.0.2

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-const MODULE_ID = 'sf2e_compendium'; // Change this ID!
+const MODULE_ID = 'sf2ecompendium'; // Change this ID!
 
 // No need to change the code below this line, but it’s your module so do it if you want!
 Hooks.once('babele.init', (babele) => {
